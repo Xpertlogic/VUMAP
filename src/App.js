07f232ff7edb344498226e1d7c240bf61bffe205
kuldeeprawat <123kuldeeprawat@gmail.com>
2024-01-10 10:15:30 +0530

    add layout

--- src/App.js
+++ src/App.js
@@ -1,147 +1,111 @@
-import BaseMap from "./components/BaseMap";
 import React, { useState } from "react";
-import { Select, Space } from "antd";
-import States from "./data/states.json";
-import Cities from "./data/BBSR.json";
 import "./style/style.css";
-import Header from "./components/Header";
-import CheckBox from "./components/CheckBox";
-
+import { Layout, Menu, Breadcrumb, Select, Form, Checkbox } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
 function App() {
-  const [stateName, setStateName] = useState([]);
-  const [cityName, setCityName] = useState([]);
-  const [centerPosition, setCenetrPosition] = useState([20.5937, 78.9629]);
-  const [category, setCategory] = useState([]);
-
-  //-------------- Enable and Disable State---------------
-  const [isCityDisabled, setIsCityDisabled] = useState(true);
-  const [isCategoryDisabled, setIsCategoryDisabled] = useState(true);
-
-  /* ---- States ---- */
-
-  const getStates = States.features
-    .map((item) => item.properties.STATE)
-    .filter((x) => x !== null);
-
-  const changeState = (value) => {
-    const getState = States?.features.find(
-      (item) => item.properties.STATE === value
-    );
-    setStateName(getState);
-    setCenetrPosition([getState?.properties.y, getState?.properties?.x]);
-    //------------------------
-    setIsCityDisabled(false);
-    setIsCategoryDisabled(true);
-    //------------------------
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
   };
-
-  /* ---- Cities ---- */
-  const getCities = Cities.features
-    .map((item) => item.properties.CITY_NAME)
-    .filter((x) => x !== null);
-
-  const changeCity = (value) => {
-    const getCity = Cities?.features.find(
-      (item) => item.properties.CITY_NAME === value
-    );
-    setCityName(getCity);
-    setCenetrPosition([getCity?.properties.y, getCity?.properties?.x]);
-    //--------------------------
-    setIsCategoryDisabled(false);
-    //--------------------------
-  };
-
-  /* ---- Category ---- */
-
-  const changeCategory = (value) => {
-    setCategory(value);
-  };
-
-  const categories = ["Buildings", "Roads", "POI"];
   return (
-    <>
-      <Header />
-      <div className="appContainer">
-        <div className="leftBarContainer">
-          <Space wrap className="m-2 grid">
-            {/* --- State --- */}
-            <label htmlFor="">State :</label>
-            <Select
-              className="w-[150px]"
-              onChange={(value) => changeState(value)}
-            >
-              {getStates && getStates !== undefined
-                ? getStates.map((stateItem, index) => {
-                    return (
-                      <option value={stateItem} key={index}>
-                        {stateItem}
-                      </option>
-                    );
-                  })
-                : "No State"}
-            </Select>
-            {/* --- City --- */}
-            <label htmlFor="">City :</label>
-            <Select
-              className="w-[150px]"
-              onChange={(value) => changeCity(value)}
-              disabled={isCityDisabled}
-            >
-              {getCities && getCities !== undefined
-                ? getCities.map((cityItem, index) => {
-                    return (
-                      <option value={cityItem} key={index}>
-                        {cityItem}
-                      </option>
-                    );
-                  })
-                : "No City"}
-            </Select>
-            {/* --- Category --- */}
-            <label htmlFor="">Category :</label>
-            <Select
-              className="w-[150px]"
-              onChange={(value) => changeCategory(value)}
-              disabled={isCategoryDisabled}
-            >
-              {categories && categories !== undefined
-                ? categories.map((item, index) => {
-                    return (
-                      <option value={item} key={index}>
-                        {item}
-                      </option>
-                    );
-                  })
-                : "No Category"}
-            </Select>
-            {/* --- Locality --- */}
-            {/* <label htmlFor="">Locality :</label>
-            <Select
-              className="w-[150px]"
-              onChange={(value) => changeCategory(value)}
-              disabled={isCategoryDisabled}
-            ></Select> */}
-            {/* --- SubLocality --- */}
-            {/* <label htmlFor="">SubLocality :</label>
-            <Select
-              className="w-[150px]"
-              onChange={(value) => changeCategory(value)}
-              disabled={isCategoryDisabled}
-            ></Select> */}
-          </Space>
-          {/* check box components  */}
-          <CheckBox />
-        </div>
-        <div className="mapContainer">
-          <BaseMap
-            data={centerPosition}
-            state={stateName}
-            city={cityName}
-            categoryName={category}
-          />
-        </div>
-      </div>
-    </>
+    <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="2">About</Menu.Item>
+        <Menu.Item key="3">Contact</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Map</Breadcrumb.Item>
+      </Breadcrumb>
+      <Layout style={{ padding: '24px 0', background: '#fff' }}>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%' }}
+          >
+            <Form.Item label="States">
+              <Select
+                style={{ width: 120 }}
+                onChange={handleChange}
+                options={[
+                  { value: 'orissa', label: 'Odissa' },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="City">
+              <Select
+                style={{ width: 120 }}
+                onChange={handleChange}
+                options={[
+                  { value: 'bhubaneswar', label: 'Bhubaneswar' },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="Postal">
+              <Select
+                style={{ width: 120 }}
+                onChange={handleChange}
+                options={[
+                  { value: 'bhubaneswar', label: 'Bhubaneswar' },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="Locality">
+              <Select
+                style={{ width: 120 }}
+                onChange={handleChange}
+                options={[
+                  { value: 'bhubaneswar', label: 'Bhubaneswar' },
+                ]}
+              />
+            </Form.Item>
+            <SubMenu key="sub1" title={<span>Transports</span>}>
+              <SubMenu key="sub1-1" title="Airports">
+                <Menu.Item key="3"><Checkbox>Internationals Airports</Checkbox></Menu.Item>
+                <Menu.Item key="4"><Checkbox>Major Airports</Checkbox></Menu.Item>
+                <Menu.Item key="1"><Checkbox>State/Other Airports</Checkbox></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub1-2" title="Rail">
+                <Menu.Item key="3"><Checkbox>Rail Line</Checkbox></Menu.Item>
+                <Menu.Item key="4"><Checkbox>Platforms</Checkbox></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub1-3" title="Roads">
+                <Menu.Item key="3"><Checkbox>Roads</Checkbox></Menu.Item>
+                <Menu.Item key="4"><Checkbox>Tollgates</Checkbox></Menu.Item>
+                <Menu.Item key="1"><Checkbox>Tunnel</Checkbox></Menu.Item>
+                <Menu.Item key="2"><Checkbox>Fly over</Checkbox></Menu.Item>
+                <Menu.Item key="5"><Checkbox>Roads Signs</Checkbox></Menu.Item>
+                <Menu.Item key="6"><Checkbox>Parking Area</Checkbox></Menu.Item>
+
+              </SubMenu>
+            </SubMenu>
+            <SubMenu key="sub2" title={<span>Buildings</span>}>
+              <Menu.Item key="5"><Checkbox>Residentials</Checkbox></Menu.Item>
+              <Menu.Item key="6"><Checkbox>Commercial</Checkbox></Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+          Content
+        </Content>
+      </Layout>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+       ©2023 Vumtech
+    </Footer>
+  </Layout>
   );
 }
 
