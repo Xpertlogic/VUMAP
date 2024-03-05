8e4692fc6ac4203d9256981d8816acac94137393
kuldeeprawat <123kuldeeprawat@gmail.com>
2024-03-05 10:38:18 +0530

    fixed selected data

--- src/components/OpenMap.jsx
+++ src/components/OpenMap.jsx
@@ -299,6 +299,7 @@ function OpenMap({
       }
 
       setTotalData(newData);
+      setMarkersInsidePolygon(newData);
     };
     getData();
   }, [selectedAirportTypes, selectedPoiTypes, selectedRailTypes]);
